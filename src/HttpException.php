90af1faeee88ee6761ac3f8adc7e183c09193cc9
George Stan <contact@georgestan.com>
2017-09-13 13:10:44 +0300

    wip

--- src/HttpException.php
+++ src/HttpException.php
@@ -7,5 +7,5 @@ use Symfony\Component\HttpKernel\Exception\HttpException as SymfonyHttpException
 class HttpException extends SymfonyHttpException
 {
     //TODO: add our custom methods here
-    //TODO: non-http exceptions are needed? probably - if so, create a more generic exception to be extended by all
+    //TODO: methods for debug level: user / developer
 }
